5d0a63b681e43039aeb930f32893697d90cb57ff
gyu.byeon <isoul902@gmail.com>
2024-07-10 09:24:58 +0900

    [U] green -> refactoring까지 한방에 해버림

--- PrimeFactor.py
+++ PrimeFactor.py
@@ -1,3 +1,24 @@
 class PrimeFactor():
-    def __init__(self, n):
-        pass
+    def __init__(self):
+        self.rst = []
+
+    def run(self, param):
+        self.rst = []
+        if param ==1:
+            self.rst = []
+            return self.rst
+        self._calc(param)
+        return self.rst
+
+    def _calc(self, param):
+        if param <= 1:
+            return
+        for num in range(2,param+1):
+            if param%num ==0:
+                self.rst.append(num)
+                self._calc(param//num)
+                return
+        self.rst.append(param)
+
+
+
--- test_PrimeFactor.py
+++ test_PrimeFactor.py
@@ -1,5 +1,24 @@
 from unittest import TestCase
 
+from PrimeFactor import PrimeFactor
+
 
 class TestPrimeFactor(TestCase):
-    pass
+    def test_case1(self):
+        pf = PrimeFactor()
+        self.assertEqual([],pf.run(1))
+
+    def test_case2(self):
+        pf = PrimeFactor()
+        self.assertEqual([2],pf.run(2))
+
+    def test_case3(self):
+        pf = PrimeFactor()
+        self.assertEqual([3],pf.run(3))
+    def test_case4(self):
+        pf = PrimeFactor()
+        self.assertEqual([2,2],pf.run(4))
+
+    def test_case5(self):
+        pf = PrimeFactor()
+        self.assertEqual([5],pf.run(5))
